b1203e7b1b9abe7f7e1eefabffe794390e1b5270
mathavan01 <mathavan2006@outlook.com>
2026-07-28 13:03:58 -0700

    Fix: restore Yosemite page, correct image path and imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="container">
     <h1 className="title">Yosemite National Park</h1>
-    <img className="scenery-img-styled" src="./scenery.jpg" alt="A beautiful scenery" />
+    <img className="scenery-img" src="/scenery.jpeg" alt="Half Dome" />
       <p className="description">
         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
       </p>
